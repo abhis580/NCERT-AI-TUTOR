beb046cb8991173937d70ebee46d65b828bfb1bb
imabhis580 <abhinavprataps580@gmail.com>
2026-08-04 15:59:01 +0530

    using text splitter to split the data into chunks

---
+++ src/splitter.py
@@ -0,0 +1,9 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from src.loader import load_documents
+documents = load_documents()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=150
+)
+
+chunks = text_splitter.split_documents(documents)
