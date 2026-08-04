bfbf87adb76b65acc9b7ce4504e0e7f45db75782
imabhis580 <abhinavprataps580@gmail.com>
2026-08-04 15:44:22 +0530

    Initial project structure

---
+++ src/loader.py
@@ -0,0 +1,23 @@
+import os
+from langchain_community.document_loaders import PyMuPDFLoader
+
+def load_documents():
+    folder_path = "data"
+
+    documents = []
+
+    for file in os.listdir(folder_path):
+
+        if file.endswith(".pdf"):
+
+            print(f"Loading: {file}")
+
+            loader = PyMuPDFLoader(os.path.join(folder_path, file))
+
+            docs = loader.load()
+
+            documents.extend(docs)
+
+    print(f"\n✅ Total Pages Loaded: {len(documents)}")
+
+    return documents
