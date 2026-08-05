b2728ce4fcde7e8f0c720330eeeb759def88180c
abhis580 <abhinavprataps580@gmail.com>
2026-08-05 11:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,4 @@ question = st.text_input("Please ask your question")
 if st.button("Ask"):
 
     answer = ragchain.invoke(question)
-    st.title("🙆🏼‍♂️")
-
-    st.write(answer)
+    st.title("💡") st.write(answer)
